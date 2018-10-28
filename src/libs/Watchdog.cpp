13b48f90d347ae7b6b1410af44103fe803f9b47c
Venelin Efremov <ghent360@iqury.us>
2018-10-28 10:38:26 -0700

    Port watchdog cleanup by Matt Baker.

--- src/libs/Watchdog.cpp
+++ src/libs/Watchdog.cpp
@@ -1,18 +1,27 @@
 #include "Watchdog.h"
 #include "Kernel.h"
 
+#ifndef __STM32F4__
 #include <lpc17xx_wdt.h>
+#else
+#include <stm32f4xx.h>
+#endif
 
 #include <mri.h>
 
 #include "gpio.h"
 extern GPIO leds[];
 
+#ifdef __STM32F4__
+static WWDG_HandleTypeDef m_wdt_handle;
+#endif
+
 // TODO : comment this
 // Basically, when stuff stop answering, reset, or enter MRI mode, or something
 
 Watchdog::Watchdog(uint32_t timeout, WDT_ACTION action)
 {
+#ifndef __STM32F4__
     WDT_Init(WDT_CLKSRC_IRC, (action == WDT_MRI)?WDT_MODE_INT_ONLY:WDT_MODE_RESET);
     WDT_Start(timeout);
     WDT_Feed();
@@ -21,11 +30,30 @@ Watchdog::Watchdog(uint32_t timeout, WDT_ACTION action)
         NVIC_EnableIRQ(WDT_IRQn);
         NVIC_SetPriority(WDT_IRQn, 1);
     }
+#else
+    m_wdt_handle.Instance         = WWDG;
+    m_wdt_handle.Init.Prescaler   = WWDG_CFR_WDGTB0 | WWDG_CFR_WDGTB1; // prescale /8
+    m_wdt_handle.Init.Window      = WWDG_CFR_W; // load max values, still timeout ~ 100ms
+    m_wdt_handle.Init.Counter     = WWDG_CR_T;  // TODO rewrite to use IWDG for longer timeouts
+    m_wdt_handle.Init.EWIMode     = (action == WDT_MRI) ? WWDG_SR_EWIF : 0;
+
+    HAL_WWDG_Init(&m_wdt_handle);
+    feed();
+    if(action == WDT_MRI) {
+        // enable the interrupt
+        NVIC_EnableIRQ(WWDG_IRQn);
+        NVIC_SetPriority(WWDG_IRQn, 1);
+    }
+#endif
 }
 
 void Watchdog::feed()
 {
+#ifndef __STM32F4__
     WDT_Feed();
+#else
+    HAL_WWDG_Refresh(&m_wdt_handle);
+#endif
 }
 
 void Watchdog::on_module_loaded()
@@ -42,6 +70,7 @@ void Watchdog::on_idle(void*)
 
 // when watchdog triggers, set a led pattern and enter MRI which turns everything off into a safe state
 // TODO handle when MRI is disabled
+#ifndef __STM32F4__
 extern "C" void WDT_IRQHandler(void)
 {
     if(THEKERNEL->is_using_leds()) {
@@ -56,3 +85,18 @@ extern "C" void WDT_IRQHandler(void)
     WDT_Feed();
     __debugbreak();
 }
+#else
+extern "C" void WWDG_IRQHandler(void)
+{
+    if(THEKERNEL->is_using_leds()) {
+        // set led pattern to show we are in watchdog timeout
+        leds[0]= 0;
+        //leds[1]= 1;
+        //leds[2]= 0;
+        //leds[3]= 1;
+    }
+    HAL_WWDG_IRQHandler(&m_wdt_handle); // clears int flag
+    HAL_WWDG_Refresh(&m_wdt_handle);
+    __debugbreak();
+}
+#endif // __STM32F4__
