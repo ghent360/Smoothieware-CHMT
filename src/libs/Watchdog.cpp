5ab404191a147ab2e8af9e10269f6916aabe00a7
Venelin Efremov <ghent360@iqury.us>
2018-10-29 23:33:27 -0700

    Update to the latest STM32 F4 HAL (V1.21.0)

--- src/libs/Watchdog.cpp
+++ src/libs/Watchdog.cpp
@@ -55,7 +55,7 @@ void Watchdog::feed()
 #ifndef __STM32F4__
     WDT_Feed();
 #else
-    HAL_WWDG_Refresh(&m_wdt_handle, WWDG_CR_T);
+    HAL_WWDG_Refresh(&m_wdt_handle);
 #endif
 }
 
@@ -99,7 +99,7 @@ extern "C" void WWDG_IRQHandler(void)
         //leds[3]= 1;
     }
     HAL_WWDG_IRQHandler(&m_wdt_handle); // clears int flag
-    //HAL_WWDG_Refresh(&m_wdt_handle, WWDG_CR_T);  // don't refresh, let wdt reset device
+    //HAL_WWDG_Refresh(&m_wdt_handle);  // don't refresh, let wdt reset device
     __debugbreak();                     // but trigger breakpoint if we're on a debugger
 }
 #endif // __STM32F4__
