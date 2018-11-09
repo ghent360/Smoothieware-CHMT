acbbe3944d5281d6e110fc34b2a2d4dfe34fdacf
Venelin Efremov <ghent360@iqury.us>
2018-11-09 00:23:18 -0800

    Setup the hardwired pins for PRNTR board V1 rev3

--- src/board_pins_prntr_v1.h
+++ src/board_pins_prntr_v1.h
@@ -1,22 +1,22 @@
 #ifndef __BOARD_PINS_PRNTR_V1_H__
 #define __BOARD_PINS_PRNTR_V1_H__
 
-#define SPI2_MOSI PA_0
-#define SPI2_MISO PA_0
-#define SPI2_SCK  PA_0
-#define SDCARD_CS PA_0
+#define SPI2_MOSI PC_1
+#define SPI2_MISO PC_2
+#define SPI2_SCK  PB_10
+#define SDCARD_CS NC
 
-#define SPI1_MOSI PA_0
-#define SPI1_MISO PA_0
-#define SPI1_SCK  PA_0
+#define SPI1_MOSI PC_1
+#define SPI1_MISO PC_2
+#define SPI1_SCK  PB_10
 
-#define I2C_SDA PA_0
-#define I2C_SCK PA_0
+#define I2C_SDA PB_9
+#define I2C_SCK PB_8
 
-#define LED1    PA_0
-#define LED2    PA_0
-#define LED3    PA_0
-#define LED4    PA_0
-#define LED5    PA_0
+#define LED1    NC
+#define LED2    NC
+#define LED3    NC
+#define LED4    NC
+#define LED5    NC
 
 #endif // __BOARD_PINS_PRNTR_V1_H__
